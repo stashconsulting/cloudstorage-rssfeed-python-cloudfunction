a60b1397d560249bc0d012b64f37375b78f1d096
syler023 <gen23.al@gmail.com>
2020-04-15 19:48:07 -0400

    updating the data to accept FormData request and saving the file into the bucket

--- insert-data.py
+++ insert-data.py
@@ -1,27 +1,47 @@
 """ This code demonstrates insert data into firestore
-
 using cloud Function with a http trigger
-
 for any POST request in the API(endpoints) .
 """
-from google.cloud import firestore
+from google.cloud import firestore, storage
 from os import environ
-import datetime
 
 client = firestore.Client()
 collection = environ.get('collection', '')
+storage_client = storage.Client()
+destination_bucket = environ.get('DESTINATION_BUCKET', '')
 
 
 def get_data(request):
-    request_json = request.get_json()
-    title = request_json['title']
-    description = request_json['description']
-    creation_date = request_json['creation_date']
-    data = {
-        u'title': title,
-        u'description': description,
-        u'creation_date': creation_date,
-        u'link': u'link',
-        u'generation_date': datetime.datetime.now()
-    }
-    client.collection(collection).document().set(data)
+    request_form = request.form
+    request_files = request.files
+
+    title = request_form['title']
+    description = request_form['description']
+    creation_date = request_form['creation_date']
+
+    for key in request_files.keys():
+        upload_blob(
+            destination_bucket=destination_bucket,
+            destination_blob_name=key,
+            content=request_files[key]
+        )
+
+
+def upload_blob(destination_bucket, content, destination_blob_name):
+    """Upload a file into the bucket destination.
+    """
+    bucket = storage_client.get_bucket(destination_bucket)
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_file(content)
+
+
+# def post_data():
+#     data = {
+#         u'title': title,
+#         u'description': description,
+#         u'creation_date': creation_date,
+#         u'link': u'link',
+#         u'generation_date': datetime.datetime.now()
+#     }
+
+#     client.collection(collection).document().set(data)

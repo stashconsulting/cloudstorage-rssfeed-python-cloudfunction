8986323905026f65df4b2e5cbd5637a69ce2568c
syler023 <gen23.al@gmail.com>
2020-06-09 16:09:37 -0400

    testing

--- main.tf
+++ main.tf
@@ -206,16 +206,16 @@ resource "null_resource" "building_docker_image" {
   }
 }
 
-data "google_iam_policy" "noauth" {
-  binding {
-    role = "roles/run.invoker"
-    members = [
-      "allUsers",
-    ]
-  }
-}
+# data "google_iam_policy" "noauth" {
+#   binding {
+#     role = "roles/run.invoker"
+#     members = [
+#       "allUsers",
+#     ]
+#   }
+# }
 
-resource "google_cloud_run_service_iam_policy" "noauth" {
-  service     = google_cloud_run_service.cloudrunsrv.name
-  policy_data = data.google_iam_policy.noauth.policy_data
-}
+# resource "google_cloud_run_service_iam_policy" "noauth" {
+#   service     = google_cloud_run_service.cloudrunsrv.name
+#   policy_data = data.google_iam_policy.noauth.policy_data
+# }
